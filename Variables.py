b19a653609e0ef1e443f067977a1ca712ec76e3c
Matia Dominguez Javier <36798108+MatJavier@users.noreply.github.com>
2023-10-23 09:53:55 -0400

    Create Variables.py
    Variables en python

---
+++ Variables.py
@@ -0,0 +1,27 @@
+#Variables type String, Int, Bool
+
+nombre = "fulano"
+edad = 25
+casado = False
+
+#formas de escribir nombres de variables correctamente en python
+
+myvariable ="Variable Legal"
+my_variable ="Legal tambien"
+_mi_variable ="Legal pero no me gusta"
+miVariable ="Legal Camel Case"
+PasCalCase= "Legal Pascal Case"
+CONSTANTES ="Escribir Constantes LEGAL_CONSTANTES"
+
+#Formas Ilegales de escribir variables en Python
+2mivariable ="ilegal"
+mi-varaible = "ilegal"
+my variable = "ilegal"
+
+#Definir variables multiplez
+
+nombre, apellido, edad = "Fulano", "Perensejo", 25
+
+#mostrar datos en pantalla
+
+print("nombre :",nombre , "Apellido : ", apellido, "Edad : ",edad)
