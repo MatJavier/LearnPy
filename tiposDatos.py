1e78a5ef278b838fd846de78476be86461aeb16f
Matia Dominguez Javier <36798108+MatJavier@users.noreply.github.com>
2023-10-23 10:27:29 -0400

    Create  tiposDatos.py
    Tipos de datos python Tuplas, Dict, List ect

---
+++ tiposDatos.py
@@ -0,0 +1,24 @@
+''''
+Tipo texto "str"
+Numeros "int , float, complex"
+Secuencias "List, Tuplas, Range"
+Mapas "Diccionarios"
+Booleanos"bool, True, False"
+Binarios "bytes, bytearray, memoryview"
+
+''''
+string = "Type: str"
+numero = 20 #int
+puntoFlotante = 2.2 #float
+numeroComplejo = 1j  #complex
+lista= ["Azul", "Rojo", "Verde", "Amarillo"] #lista
+tupla = ("Pina", "Mango", "Manzana") #tabla
+diccionario = {"Nombre": "Juaquin","Edad": 25, "Apellido": "Marmolejo"} #Diccionario
+rango = (10) #rango
+fr =frozenset({"Manzana", "Banana", "Cherry"}) #frozenset 
+booleano =True #Bool
+b= b"Hello" #bytes
+b= bytearry(5) #bytearry
+memory = memoryview(bytes(5))#MemoryView
+vacio = None #Nonetype
+
