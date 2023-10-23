ede0402c03f30319ab4d12a884209fff4c84ccc9
Matia Dominguez Javier <36798108+MatJavier@users.noreply.github.com>
2023-10-23 11:24:56 -0400

    Create StringMethods.py

---
+++ StringMethods.py
@@ -0,0 +1,21 @@
+#Metodos String
+mayusculas = "Esta funcion pone las letras mayusculas"
+print(mayusculas.upper())# Function UPPER
+
+minusculas = "ESTA FUNCION PONE LAS LETRAS MINUSCULAS"
+print(minusculas.lower()) # Function LOWER
+
+removeSpaces = "   Hey mis espacios son eliminados al principio y al final    "
+print(removeSpaces.strip()) #Function Strip quita los espacios
+
+replaceString = "El texto sera reemplazado por: "
+print(replaceString.replace("Por este TEXTO")) #Function Replace
+
+separador = "Separar el texto con split , si tu quiere"
+print(separador.split("/"))
+
+'''
+Link para ver mas metodos String de python
+
+'''
+url = "https://www.w3schools.com/python/python_strings_methods.asp"
