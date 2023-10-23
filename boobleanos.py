8240190da2223c884f390ea56deb7671a24ef311
Matia Dominguez Javier <36798108+MatJavier@users.noreply.github.com>
2023-10-23 17:27:01 -0400

    Create boobleanos.py
    Falta informacion para los bool

---
+++ boobleanos.py
@@ -0,0 +1,24 @@
+#Boobleanos
+a = 10
+b = 20
+
+if a > b:
+  print("A Es mayor que B")
+else:
+  print("A se equivoco no es mayor")
+
+while a > 100:
+  print("OLA")
+  break 
+
+
+
+
+#Todo esto da Falso
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
