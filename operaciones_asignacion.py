4e7a76b95757aca1d8292cdc8f51d4d09928aa24
Matia Dominguez Javier <36798108+MatJavier@users.noreply.github.com>
2023-10-30 12:47:27 -0400

    Create operaciones_asignacion.py
    operaciones en asignacion

---
+++ operaciones_asignacion.py
@@ -0,0 +1,33 @@
+#Operaciones en asignacion
+
+
+a = 10
+a += 5
+
+print(a)
+
+b = 20
+b -= 5
+
+print(b)
+
+c= 40
+c /= 2
+
+print(c)
+
+d= 5
+
+d *= 1
+
+print(d)
+
+e = 8
+e **= 3
+print(e)
+
+f= 10
+f %= 3
+
+print(f)
+
